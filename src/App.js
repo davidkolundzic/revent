9c71dd62cb1107c0d3e34d0d7e5a6c89626d40f0
David Kolundzic <david_k42@hotmail.com>
2019-03-11 10:52:04 +0100

    End of Section 03

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 // import { Button } from 'semantic-ui-react';
 import EventDashboard from "./app/features/event/EventDashboard/EventDashboard";
 import Navbar from "./app/features/nav/navbar";
+import { Container } from "semantic-ui-react";
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <EventDashboard/>
+        <Navbar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
